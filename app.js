8c96295b31f204222b486ddb282934fd1be611fc
Pavan Kalamkar <pavankalamkar34@gmail.com>
2024-03-09 11:30:33 +0530

    API updated and functions improvement

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -9,57 +9,82 @@ const msg = document.querySelector(".msg");
 
 
 for (let select of dropdowns) {
-    for (currcode in countryList) {
+  for (currcode in countryList) {
 
-        let newOption = document.createElement("option");
-        newOption.innerText = currcode;
-        newOption.value = currcode;
+    let newOption = document.createElement("option");
+    newOption.innerText = currcode;
+    newOption.value = currcode;
 
-        if (select.name === "from" && currcode === "USD") {
-            newOption.selected = "selected";
-        } else if (select.name === "to" && currcode === "INR") {
-            newOption.selected = "selected";
-        }
-
-        select.append(newOption);
+    if (select.name === "from" && currcode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currcode === "INR") {
+      newOption.selected = "selected";
     }
 
-    select.addEventListener("change", (evt) => {
-        updateFlag(evt.target);
-    })
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  })
 }
 
+
+/*------------Function to update Flags------------------*/
 const updateFlag = (element) => {
-    let currCode = element.value;
-    let countryCode = countryList[currCode];
-    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    let img = element.parentElement.querySelector("img");
-    img.src = newSrc;
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
 }
 
 
-const updateExchangeRate = async () => {
+/*------------Function to update Exchangerate------------------*/
+const updateExchangeRate = (data, amtval) => {
+  let FinalAmout = data.result;
+  msg.innerText = `${amtval} ${fromCurr.value} = ${FinalAmout} ${toCurr.value}`;
+}
 
-    let amount = document.querySelector(".amount input ");
-    let amtval = amount.value;
-    if (amtval === "" || amtval < 1) {
-        amtval = 1;
-        amount.value = "1";
+/*------------Function to get Exchangerate------------------*/
+async function getexchangeRate() {
+
+  let amount = document.querySelector(".amount input ");
+  let amtval = amount.value;
+
+  if (amtval === "" || amtval < 1) {
+    amtval = 1;
+    amount.value = "1";
+  }
+
+  const url = `${BASE_URL}from=${fromCurr.value.toLowerCase()}&to=${toCurr.value.toLowerCase()}&amount=${amtval}`;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '7e894ab668mshd5b079f71fa9a8dp1a843bjsn2da249536775',
+      'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
     }
+  };
 
-    const url = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    let data = JSON.parse(result);
+    updateExchangeRate(data, amtval);
 
-    let response = await fetch(url);
-    let data = await response.json();
-
-    let rate = data[toCurr.value.toLowerCase()];
-    let FinalAmout = amtval * rate;
-    msg.innerText = `${amtval} ${fromCurr.value} = ${FinalAmout} ${toCurr.value}`;
+  } catch (error) {
+    console.error(error);
+    alert("Try Again !");
+  }
 }
 
 
-window.addEventListener("load", updateExchangeRate);
+window.addEventListener("load", () => {
+  getexchangeRate();
+});
+
 btn.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    updateExchangeRate();
+  evt.preventDefault();
+  getexchangeRate();
 })
